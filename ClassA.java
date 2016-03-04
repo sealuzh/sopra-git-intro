18d40807e80d7fe697c5799e3532dfb0460c7b28
Genc Mazlami <genc.mazlami@uzh.ch>
2016-03-04 15:23:06 +0100

    Add name field to ClassA.

--- ClassA.java
+++ ClassA.java
@@ -3,8 +3,14 @@ public class ClassA{
 	//this is a comment
 	private int someVariable = 10;
 
+	private final String name;
+
 	public int getSomeVariable(){
 		return someVariable;
+	}
+
+	public String getName(){
+		return name;
 	}
 
 }
